947a540a4373c1ee396e2c1c0014dfde37bc90c8
DESKTOP-490CFTI\Admin <nolan974pro@gmail.com>
2024-10-29 16:20:18 +0100

    add settings constant

---
+++ lib/src/models/settings.dart
@@ -0,0 +1,6 @@
+class Settings {
+  static const defaultPadding = 10.0;
+  static const textFontSize = 10.0;
+  static const secondaryTitleFontSize = 25.0;
+  static const mainTitleFontSize = 35.0;
+}
